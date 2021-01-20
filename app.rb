fc368de5ce8e9e319d8402adb6fac86d46fcff74
Olek <forgemest@gmail.com>
2021-01-20 23:55:41 +0200

    simple name parameter validation

--- app.rb
+++ app.rb
@@ -3,28 +3,31 @@ require 'sinatra'
 # require 'sinatra/reloader'
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
 get '/about' do
-	erb :about
+  @error = 'Something wrong!'
+  erb :about
 end
 
 get '/visit' do
-	erb :visit
+  erb :visit
 end
 
 post '/visit' do
 
+    @username = params[:username]
+    @phone = params[:phone]
+    @pdatetime = params[:datetime]
+    @barber = params[:barber]
+          @color = params[:color]
 
-@username = params[:username]
-@phone = params[:phone]
-@pdatetime = params[:datetime]
-@barber = params[:barber]
-@color = params[:color]
-
-erb "ok, username is #{@username}, #{@phone}, #{@pdatetime}, #{@barber}, #{@color}"
-
+  if @username == ''
+    @error = 'Введите Имя'
+return erb :visit
+  end
+  erb "ok, username is #{@username}, #{@phone}, #{@pdatetime}, #{@barber}, #{@color}"
 end
 
 
